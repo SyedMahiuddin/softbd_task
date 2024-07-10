fd2f549528a29593951d7e13f8382c51e0b90226
Syed Mahiuddin <syedmahiuddin01@gmail.com>
2024-07-10 10:41:11 +0600

    bottom icons

---
+++ lib/Controller/view_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class ViewController extends GetxController{
+  var pageIndex=0.obs;
+  
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -1,3 +1,11 @@
-class ColorHelper{
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
+class ColorHelper{
+  static const Color darkGreen= Color(0xFF336F4A);
+  static const Color lightGreen= Color(0xFF86B560);
+  static const Color textRed= Color(0xFFFF737A);
+  static const Color darkBlack= Color(0xFF202020);
+  static const Color lightBlack= Color(0xFF6A6A6A);
 }
